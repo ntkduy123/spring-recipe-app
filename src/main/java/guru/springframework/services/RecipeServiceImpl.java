cf76c9206d5f6eaad8042e8fe9b4f610996eb14c
Duy Nguyễn <duynguyen@Duys-MacBook-Pro.local>
2021-06-21 21:16:12 +0700

    add project lombok

--- src/main/java/guru/springframework/services/RecipeServiceImpl.java
+++ src/main/java/guru/springframework/services/RecipeServiceImpl.java
@@ -2,6 +2,7 @@ package guru.springframework.services;
 
 import guru.springframework.domain.Recipe;
 import guru.springframework.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
@@ -10,6 +11,7 @@ import java.util.Set;
 /**
  * Created by jt on 6/13/17.
  */
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
